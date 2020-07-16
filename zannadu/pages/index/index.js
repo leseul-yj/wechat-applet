7f59cfdf9cbe1f6d91a8c98c574162a61c4c6e38
leseul-yj <79645550@qq.com>
2020-07-16 23:57:44 +0800

    添加行程预定

--- zannadu/pages/index/index.js
+++ zannadu/pages/index/index.js
@@ -4,73 +4,92 @@ Page({
    * 页面的初始数据
    */
   data: {
-    binnerList:[{
+    binnerList: [{
       url: "https://cdn-photos.zanadu.cn/resources/zanadu/page/7/photo_7_7_958.jpg!1920x630?v=1594606072"
-    },{
+    }, {
       url: "https://cdn-photos.zanadu.cn/resources/zanadu/page/7/photo_7_7_859.jpg!1920x630?v=1579080251"
-    },
-  {
-    url:"https://cdn-photos.zanadu.cn/resources/zanadu/page/7/photo_7_7_785.jpg!1920x630?v=1564366805"
-  },
-{
-  url: "https://cdn-photos.zanadu.cn/resources/zanadu/page/7/photo_7_7_917.jpg!1920x630?v=1590392005"
-},
-]
+    }, {
+      url: "https://cdn-photos.zanadu.cn/resources/zanadu/page/7/photo_7_7_785.jpg!1920x630?v=1564366805"
+    }, {
+      url: "https://cdn-photos.zanadu.cn/resources/zanadu/page/7/photo_7_7_917.jpg!1920x630?v=1590392005"
+    }],
+    itineraryList: [{
+      url: "https://cdn-img.zanadu.cn/assets/9c31fce6/public/images/channel/square.jpg",
+      title: "探索世界"
+    }, {
+      url: "",
+      title: "海外自由行"
+    }, {
+      url: "",
+      title: "轻奢小团"
+    }, {
+      url: "",
+      title: "大航海时代"
+    }, {
+      url: "",
+      title: "精品别墅"
+    }, {
+      url: "",
+      title: "国内短假"
+    }, {
+      url: "",
+      title: "私人定制"
+    }]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    
+  onLoad: function(options) {
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-    
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-    
+  onShow: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-    
+  onHide: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-    
+  onUnload: function() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-    
+  onPullDownRefresh: function() {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-    
+  onReachBottom: function() {
+
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-    
+  onShareAppMessage: function() {
+
   }
 })
